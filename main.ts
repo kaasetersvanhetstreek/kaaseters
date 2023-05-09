e0fdb22f3ef4483ba59431887d67f0932b60cb55
kaasetersvanhetstreek <133000099+kaasetersvanhetstreek@users.noreply.github.com>
2023-05-09 11:44:51 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    music.playMelody("G F F G G F F G ", 158)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
